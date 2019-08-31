249a0f91b5a4180868711843190a8b438e3bcc4f
ganyavhad <36654727+ganyavhad@users.noreply.github.com>
2019-09-01 00:10:50 +0530

    event added for changed in file

--- server.js
+++ server.js
@@ -3,13 +3,32 @@ const app = express();
 const util = require('util')
 const fs = require('fs')
 
+
 // app.get("/", function (req, res) {
 //     res.send("done")
 // })
 
+var getFileData = function () {
+    fs.readFile('log/logger.log', function (err, data) {
+        if (err) throw err;
+        var array = data.toString().split("\n");
+        var newAraay = [];
+        console.log("array.length ", array.length)
+        if (array.length > 10) {
+            newAraay = array.splice(array.length - 10, array.length + 1)
+        } else {
+            newAraay = array;
+        }
+        newAraay.forEach(function (log) {
+            console.log(log)
+        })
+    });
+}
+
+
 fs.watch('log/logger.log', (eventType, filename) => {
     if (eventType == 'change' && filename == 'logger.log') {
-        console.log(eventType, filename)
+        getFileData();
     }
 });
 
