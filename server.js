800b78f80c175bdc4e5044be5a63ee1e400c8f43
ganyavhad <36654727+ganyavhad@users.noreply.github.com>
2019-09-01 00:38:41 +0530

    read file with tail command

--- server.js
+++ server.js
@@ -2,34 +2,45 @@ const express = require('express');
 const app = express();
 const util = require('util')
 const fs = require('fs')
+const cp = require('child_process');
 
 
 // app.get("/", function (req, res) {
 //     res.send("done")
 // })
 
-var getFileData = function () {
-    fs.readFile('log/logger.log', function (err, data) {
-        if (err) throw err;
-        var array = data.toString().split("\n");
-        var newAraay = [];
-        console.log("array.length ", array.length)
-        if (array.length > 10) {
-            newAraay = array.splice(array.length - 10, array.length + 1)
-        } else {
-            newAraay = array;
+// var getFileData = function () {
+//     fs.readFile('log/logger.log', function (err, data) {
+//         if (err) throw err;
+//         var array = data.toString().split("\n");
+//         var newAraay = [];
+//         console.log("array.length ", array.length)
+//         if (array.length > 10) {
+//             newAraay = array.splice(array.length - 10, array.length + 1)
+//         } else {
+//             newAraay = array;
+//         }
+//         newAraay.forEach(function (log) {
+//             console.log(log)
+//         })
+//     });
+// }
+
+// with tail command
+getFileData = function () {
+    cp.exec('tail -n 5 ' + 'log/logger.log --lines 10', function (err, stdout, stderr) {
+        console.log("1111")
+        if (err) {
+            console.log(err)
         }
-        newAraay.forEach(function (log) {
-            console.log(log)
-        })
+        else {
+            console.log(stdout)
+        }
     });
 }
 
-
-fs.watch('log/logger.log', (eventType, filename) => {
-    if (eventType == 'change' && filename == 'logger.log') {
-        getFileData();
-    }
+fs.watchFile('log/logger.log', (curr, prev) => {
+    getFileData()
 });
 
 
