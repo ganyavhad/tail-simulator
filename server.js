a5f1db513809934f838a1c5341ebba2e5e257985
ganyavhad <36654727+ganyavhad@users.noreply.github.com>
2019-08-31 23:26:26 +0530

    Watch added for logger file

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const app = express();
+const util = require('util')
+const fs = require('fs')
 
-app.get("/", function (req, res) {
-    res.send("done")
-})
+// app.get("/", function (req, res) {
+//     res.send("done")
+// })
 
-app.listen(3000, function () {
+fs.watch('log/logger.log', (eventType, filename) => {
+    if (eventType == 'change' && filename == 'logger.log') {
+        console.log(eventType, filename)
+    }
+});
+
+
+app.listen(3000, () => {
     console.log("Server is running on 3000")
 })
