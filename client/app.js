c3bd5688e340f3c7245944643afeccfb027aaea4
ganyavhad <36654727+ganyavhad@users.noreply.github.com>
2019-09-01 10:36:23 +0530

    html setup for viewing logs

--- client/app.js
+++ client/app.js
@@ -1,1 +1,10 @@
-var myApp = angular.module("myApp", []);
+var myApp = angular.module("myApp", ["ui.router"]);
+
+myApp.config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider.state("logs", {
+        url: "/logs",
+        templateUrl: "client/view/logs.html",
+        controller: "Controller"
+    });
+    $urlRouterProvider.otherwise("/logs");
+});
